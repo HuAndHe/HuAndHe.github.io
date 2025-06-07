fba119042f98545d28e3df02e43c7d5ed413be22
pan-hu <pan.hu@siemens.com>
2025-06-07 14:34:30 +0800

    fix

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,32 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import * as path from 'path'
 
 
 // https://vite.dev/config/
-
+console.log(path.resolve(__dirname))
 const viteConfig=defineConfig(()=>{
   return{
     plugins: [react()],
     base:'./',
-    extensions: ['.js','.mjs','.jsx','.css']
+    extensions: ['.js','.mjs','.jsx','.css'],
+    build: {
+      rollupOptions: {
+        output: {
+          entryFileNames: 'assets/[name].js',
+          chunkFileNames: 'assets/[name].js',
+          assetFileNames: 'assets/[name].[ext]',
+          manualChunks(id) {
+            if (id.includes('node_modules')) {
+              return 'vendor'
+            }
+          }
+      },
+      input: 'src/index.html'
+      
+    }
   }
+}
 })
 
 
